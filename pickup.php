64bbb010bdc6feaab4b638f3720c8ce78fc1d15e
Jesse Poland <jpoland25@gmail.com>
2018-10-23 15:38:10 -0600

    Update pickup.php

--- pickup.php
+++ pickup.php
@@ -19,10 +19,10 @@ require_once "valet/src/config.php";
 	$mail->Host = "smtp.mailgun.org";
 	$mail->Encoding = "7bit";       // SMS uses 7-bit encoding
  	/* ------------------- Authenticate ------------------- */
-	$mail->Username   = "postmaster@sandboxec2dd140d60648d28ded9010a2bc515a.mailgun.org"; // Login
-	$mail->Password   = "f1401d7544755182e6e3e857b35a3def-bd350f28-a2b16b54"; // Password
-	$mail->From = 'Pickups@DrycleaningbyDave.ca';
-	$mail->FromName = 'Jeff Mills';
+	$mail->Username   = " "; // Login
+	$mail->Password   = " "; // Password
+	$mail->From = " ";
+	$mail->FromName = " ";
 	
 	/* ~~~ Form Submission Data ~~~ */	
 
@@ -47,7 +47,7 @@ require_once "valet/src/config.php";
 			/* ~~~ Send Email ~~~ */
 		try {
 			//Content
-			$mail->AddAddress = "4036091161@msg.telus.com"; // Where to send it
+			$mail->AddAddress = "4037149727@msg.telus.com"; // Where to send it
 			$mail->isHTML(true);	// Set email format to HTML
 			$mail->Subject = "Pickup Request";
 			$mail->Body    = $name . " has a pickup request. <br>Address: " . $addy . "<b>Pickup for </b>";
@@ -89,7 +89,7 @@ require_once "valet/src/config.php";
 			/* ~~~ Send Email ~~~ */
 	/*		try {
 				//Content
-				$mail->AddAddress = "4036091161@msg.telus.com"; // Where to send it
+				$mail->AddAddress = "4037149727@msg.telus.com"; // Where to send it
 				$mail->isHTML(true);	// Set email format to HTML
 				$mail->Subject = "Hotel Pickup Request";
 				$mail->Body    = $name . " has a pickup request. <br>Address: " . $addy . "<b>Pickup for </b>";
@@ -110,7 +110,7 @@ require_once "valet/src/config.php";
 			/* ~~~ Send Email ~~~ */
 			try {
 				//Content
-				$mail->AddAddress = "4036091161@msg.telus.com"; // Where to send it
+				$mail->AddAddress = "4037149727@msg.telus.com"; // Where to send it
 				$mail->isHTML(true);	// Set email format to HTML
 				$mail->Subject = "Hotel Pickup Request";
 				$mail->Body    = $name . " has a pickup request. <br>Address: " . $addy . "<b>Pickup for </b>";
@@ -132,7 +132,7 @@ require_once "valet/src/config.php";
 			/* ~~~ Send Email ~~~ */
 			try {
 				//Content
-				$mail->AddAddress = "4036091161@msg.telus.com"; // Where to send it
+				$mail->AddAddress = "4037149727@msg.telus.com"; // Where to send it
 				$mail->isHTML(true);	// Set email format to HTML
 				$mail->Subject = "Hotel Pickup Request";
 				$mail->Body    = $name . " has a guest valet request. <br>Address: " . $addy . "<b>Pickup for </b>";
@@ -153,7 +153,7 @@ require_once "valet/src/config.php";
 			/* ~~~ Send Email ~~~ */
 	/*		try {
 				//Content
-				$mail->AddAddress = "4036091161@msg.telus.com"; // Where to send it
+				$mail->AddAddress = "4037149727@msg.telus.com"; // Where to send it
 				$mail->isHTML(true);	// Set email format to HTML
 				$mail->Subject = "Hotel Pickup Request";
 				$mail->Body    = $name . " has a guest valet request. <br>Address: " . $addy . "<b>Pickup for </b>";
@@ -174,7 +174,7 @@ require_once "valet/src/config.php";
 			/* ~~~ Send Email ~~~ */
 			try {
 				//Content
-				$mail->AddAddress = "4036091161@msg.telus.com"; // Where to send it
+				$mail->AddAddress = "4037149727@msg.telus.com"; // Where to send it
 				$mail->isHTML(true);	// Set email format to HTML
 				$mail->Subject = "Test Hotel Pickup Request";
 				$mail->Body    = $name . " has a guest valet request. <br>Address: " . $addy . "<b>Pickup for </b>";
@@ -193,7 +193,7 @@ require_once "valet/src/config.php";
 			/* ~~~ Send Email ~~~ */
 			try {
 				//Content
-				$mail->AddAddress = "4036091161@msg.telus.com"; // Where to send it
+				$mail->AddAddress = "4037149727@msg.telus.com"; // Where to send it
 				$mail->isHTML(true);	// Set email format to HTML
 				$mail->Subject = "Test Staff Hotel Pickup Request";
 				$mail->Body    = $name . " has a staff pickup request. <br>Address: " . $addy . "<b>Pickup for </b>";
