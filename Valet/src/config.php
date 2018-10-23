2adc58c51ea781055d05906a2e9652379033fbc7
Jesse Poland <jpoland25@gmail.com>
2018-10-23 12:07:12 -0600

    Add files via upload

--- Valet/src/config.php
+++ Valet/src/config.php
@@ -1,1 +1,19 @@
-
+<?php
+	/* ------------------- PDO Login Information ------------------- */	
+	define('DB_SERVER', 'localhost');
+	define('DB_USERNAME', ' ');
+	define('DB_PASSWORD', ' ');
+	define('DB_NAME', ' ');
+	/* ------------------- PDO connect to database ------------------- */
+	try{
+		$pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
+    	// Set the PDO error mode to exception
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	} catch(PDOException $e){
+		die("ERROR: Could not connect. " . $e->getMessage());
+	}
+	/* ------------------- Include Mailer ---------------------- */
+	require "valet/src/mailer/Exception.php";
+	require "valet/src/mailer/PHPMailer.php";
+	require "valet/src/mailer/SMTP.php";
+?>
